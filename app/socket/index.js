b30d27983f6a18363ccb2facb291fa68fde23612
mekladious <miraekladious@gmail.com>
2019-05-02 22:57:53 +0200

    fixed p2p error

--- app/socket/index.js
+++ app/socket/index.js
@@ -6,7 +6,7 @@ var adapter = require('socket.io-redis');
 
 var Room = require('../models/room');
 
-var p2p = require('socket.io-p2p-server').Server;
+// var p2p = require('socket.io-p2p-server').Server;
 
 /**
  * Encapsulates all code for emitting and listening to socket events
@@ -123,8 +123,8 @@ var init = function(app){
 	var server 	= require('http').Server(app);
 	var io 		= require('socket.io')(server);
 
-	var p2p = require('socket.io-p2p-server').Server;
-	io.use(p2p);
+	// var p2p = require('socket.io-p2p-server').Server;
+	// io.use(p2p);
 	
 	// Force Socket.io to ONLY use "websockets"; No Long Polling.
 	io.set('transports', ['websocket']);
